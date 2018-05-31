513fadbb71eb5305a353c6b75ba534672d5a83c8
Kritteam <39475181+Kritteam@users.noreply.github.com>
2018-05-31 22:53:49 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => '134e0077d4f310588c20c45a008486ab',
+        'to' => 'Ue77a191627f6ac91899e75d92264310c',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
