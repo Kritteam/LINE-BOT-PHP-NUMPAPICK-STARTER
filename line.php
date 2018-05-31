602b64c263df8b990acb35c4c0f5fe537defc821
Kritteam <39475181+Kritteam@users.noreply.github.com>
2018-05-31 17:38:41 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'qdD3zmIWVXIuS4KBEjMPVAnr6wUy6bC5m5WSy9A9ouBfDV/P1lSg85ZNv6ei16MVGhFh5qfAxiEcgvC+bjIouKZPiSZAGfxuVNIspZkyDOsukXuBj1HAiS0EqULQeLbEau9cf9/WuQ06e2PfPL0W8QdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => '134e0077d4f310588c20c45a008486ab',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
